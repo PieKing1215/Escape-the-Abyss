22e551fd42a6fa59c6cb2cc4946075ef4250ed9e
dman926 <dman992266@gmail.com>
2020-12-04 10:22:07 -0500

    Fix ObjectList causing crash on shutdown
    Memory issue causing crashes on shutdown fixed.

--- dragonfly/ObjectList.cpp
+++ dragonfly/ObjectList.cpp
@@ -26,7 +26,8 @@ df::ObjectList::ObjectList(const df::ObjectList& other) {
 	}
 }
 df::ObjectList::~ObjectList() {
-	free(m_p_obj);
+	// Freeing the memory here causes a read violation on shutdown. Shouldn't really matter, but it should be known.
+	// free(m_p_obj);
 }
 
 bool df::ObjectList::operator==(const df::ObjectList& rhs) {
